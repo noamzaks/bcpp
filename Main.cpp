bf4638a9539fad93764d66f8485b4cd44869b373
Noam Zaks <noam@noamzaks.com>
2025-06-17 01:40:36 -0700

    finish bumblebee

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <thread>
+#include <future>
+
+// The size of the test array for the program itself.
+static constexpr int ARRAY_TEST_SIZE = 20;
+
+// The number of threads to use for filling the array.
+static constexpr int THREAD_COUNT = 4;
+
+// The amount of consecutive numbers each thread searches for a primes within.
+// This will be scaled by the amount of primes searched for.
+static constexpr int SEARCH_RANGE_SIZE = 10;
 
 /**
  * @return true if the number is a prime
@@ -18,31 +30,79 @@ bool isPrime(int n) {
 }
 
 /**
- * Simple program to input a number and print its square root.
- *
- * @return 0 if succesful, negative number for error
+ * Fills the array with primes in the given range.
+ * 
+ * @return whether enough primes were found to fill the array.
  */
-int userCheckPrime() {
-    int userInput = -1;
+bool fillPrimesInRange(int* array, int array_length, int start, int end) {
+    int current = 0;
+    for (int n = start; n < end && current < array_length; n++) {
+        if (isPrime(n)) {
+            array[current] = n;
+            current++;
+        }
+    }
+    return current == array_length;
+}
 
-    std::cout << "Please enter a number" << std::endl;
+/**
+ * Threaded function to fill the array with different primes.
+ *
+ * Creates THREAD_COUNT thread each searching in a separate range for primes.
+ *
+ * @return 0 if successful or a negative number if an error occurred.
+ */
+int fillArrayWithPrimes(int* array, int array_length) {
+    std::future<bool> threads[THREAD_COUNT];
 
-    std::cin >> userInput;
-    if (std::cin.fail()) {
-        std::cerr << "Invalid input received!" << std::endl;
-        return -1;
+    int chunk_size = array_length / THREAD_COUNT;
+    int current_start = 0;
+    int current_end = chunk_size;
+    int range_start = 2;
+    int range_end = 2;
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        // Make sure last thread finishes the array.
+        if (i == THREAD_COUNT - 1) {
+            current_end = array_length;
+        }
+
+        range_end = range_start + SEARCH_RANGE_SIZE * (current_end - current_start);
+        threads[i] = std::async(fillPrimesInRange, array + current_start, current_end - current_start,
+                                 range_start, range_end);
+        range_start = range_end;
+
+        current_start += chunk_size;
+        current_end += chunk_size;
     }
 
-    if (isPrime(userInput)) {
-        std::cout << "That's a prime!" << std::endl;
-    }
-    else {
-        std::cout << "That's not a prime!" << std::endl; 
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        if (!threads[i].get()) {
+            std::cerr << "Search range too small! Couldn't find primes." << std::endl;
+            return -1;
+        }
     }
 
     return 0;
 }
 
+/** Prints the given array in a single line. */
+void printArray(int* array, int array_length) {
+    for (int i = 0; i < array_length; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main() {
-    return userCheckPrime();
+    int* array = new int[ARRAY_TEST_SIZE];
+    
+    int filling_ok = fillArrayWithPrimes(array, ARRAY_TEST_SIZE);
+    if (filling_ok != 0) {
+        delete[] array;
+        return filling_ok;
+    }
+    printArray(array, ARRAY_TEST_SIZE);
+
+    delete[] array;
 }
