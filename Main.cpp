55d87a77e6a2ff7cd670b39995c58276eede6de4
Noam Zaks <noam@noamzaks.com>
2025-06-18 12:37:41 +0300

    fix 7

--- Main.cpp
+++ Main.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 int main() {
     // 1
-    vector<int> numbers = {8, 4, 1, 2, 3, 10, 4, 5, 4, 1574};
+    vector<int> numbers = {8, 4, 1, 2, 3, 10, 4, 5, 4, 1574, 1582};
 
     // 2
-    cout << count_if(numbers.begin(), numbers.end(), bind(equal_to<int>(), placeholders::_1, 4)) << endl;
+    cout << count_if(numbers.begin(), numbers.end(), bind(equal_to<>(), placeholders::_1, 4)) << endl;
 
     // 3
-    cout << count_if(numbers.begin(), numbers.end(), bind(greater<int>(), placeholders::_1, 4)) << endl;
+    cout << count_if(numbers.begin(), numbers.end(), bind(greater<>(), placeholders::_1, 4)) << endl;
 
     // 4
-    numbers.erase(remove_if(numbers.begin(), numbers.end(), bind(less<int>(), placeholders::_1, 8)), numbers.end());
+    numbers.erase(remove_if(numbers.begin(), numbers.end(), bind(less<>(), placeholders::_1, 8)), numbers.end());
 
     // 5
     copy(numbers.begin(), numbers.end(), ostream_iterator<int>(cout, " "));
@@ -26,12 +26,11 @@ int main() {
     // 6
     vector<int> moreNumbers;
 
-    // 7 - is the intention to remove the first element?
-    adjacent_difference(numbers.begin(), numbers.end(), back_inserter(moreNumbers));
+    transform(next(numbers.begin()), numbers.end(), numbers.begin(), back_inserter(moreNumbers), std::minus<>());
 
     // 8
     copy(moreNumbers.begin(), moreNumbers.end(), ostream_iterator<int>(cout, " "));
 
     // 9
-    cout << accumulate(moreNumbers.begin(), moreNumbers.end(), 0, plus<int>()) << endl;
+    cout << accumulate(moreNumbers.begin(), moreNumbers.end(), 0, plus<>()) << endl;
 }
